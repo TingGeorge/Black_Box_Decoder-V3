748be35e8de60eb7848f527a5ffe2b5bbf0aae78
丁肇志 <104309438+TingGeorge@users.noreply.github.com>
2025-07-19 00:18:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def login():
         # Ensure the "Name" column is of string type for comparison
         st.session_state.user = "USER " + str(st.session_state.username_input)
         st.session_state.existing_data["Name"] = st.session_state.existing_data["Name"].astype(str)
-        
+
         if st.session_state.user not in st.session_state.existing_data["Name"].values:
             new_data = pd.DataFrame(
                 [
@@ -92,7 +92,50 @@ def login():
             st.session_state.index = new_pd.shape[0] - 1  # number of rows
         else:
             st.session_state.index = st.session_state.existing_data.index[st.session_state.existing_data["Name"] == st.session_state.user].tolist()[0]
-        
+
+        CORRECT_ANSWER_1 = st.secrets["answers"]["level_1"]
+        CORRECT_ANSWER_2 = st.secrets["answers"]["level_2"]
+        CORRECT_ANSWER_3 = st.secrets["answers"]["level_3"]
+
+        if st.session_state.level_1_correct:
+            if not st.session_state.score_calculated_1:
+                st.session_state.score += 1
+                st.session_state.score_calculated_1 = True
+        elif st.session_state.existing_data.loc[st.session_state.index, "Level 1"] == CORRECT_ANSWER_1:
+            if not st.session_state.score_calculated_1:
+                st.session_state.score += 1
+                st.session_state.score_calculated_1 = True
+
+            st.session_state.text_1 = CORRECT_ANSWER_1
+            st.session_state.level_1_correct = True
+            st.session_state.fail_answer_1 = False
+
+        if st.session_state.level_2_correct:
+            if not st.session_state.score_calculated_2:
+                st.session_state.score += 1
+                st.session_state.score_calculated_2 = True
+        elif st.session_state.existing_data.loc[st.session_state.index, "Level 2"] == CORRECT_ANSWER_2:
+            if not st.session_state.score_calculated_2:
+                st.session_state.score += 1
+                st.session_state.score_calculated_2 = True
+
+            st.session_state.text_2 = CORRECT_ANSWER_2
+            st.session_state.level_2_correct = True
+            st.session_state.fail_answer_2 = False
+
+        if st.session_state.level_3_correct:
+            if not st.session_state.score_calculated_3:
+                st.session_state.score += 1
+                st.session_state.score_calculated_3 = True
+        elif st.session_state.existing_data.loc[st.session_state.index, "Level 3"] == CORRECT_ANSWER_3:
+            if not st.session_state.score_calculated_3:
+                st.session_state.score += 1
+                st.session_state.score_calculated_3 = True
+
+            st.session_state.text_3 = CORRECT_ANSWER_3
+            st.session_state.level_3_correct = True
+            st.session_state.fail_answer_3 = False
+
         st.cache_data.clear()
 
     with st.form("login_form", border=False):
